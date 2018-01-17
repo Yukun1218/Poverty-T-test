393761d43978806746591474dd7fe3d9c8c03d1c
Yukun1218 <daisywang@gwu.edu>
2018-01-17 11:23:26 -0500

    Add data source

--- Data.R
+++ Data.R
@@ -1,2 +1,16 @@
 setwd('C:/Users/wb516609/Desktop/Poverty-T-competition')
 
+A_hhold_train <- read.csv('C:/Users/wb516609/Desktop/Poverty-T-competition/A_hhold_train.csv', header = T)
+A_hhold_test <- read.csv('C:/Users/wb516609/Desktop/Poverty-T-competition/A_hhold_test.csv', header = T)
+A_indiv_train <- read.csv('C:/Users/wb516609/Desktop/Poverty-T-competition/A_indiv_train.csv', header = T)
+A_indiv_test <- read.csv('C:/Users/wb516609/Desktop/Poverty-T-competition/A_indiv_test.csv', header = T)
+
+B_hhold_train <- read.csv('C:/Users/wb516609/Desktop/Poverty-T-competition/B_hhold_train.csv', header = T)
+B_hhold_test <- read.csv('C:/Users/wb516609/Desktop/Poverty-T-competition/B_hhold_test.csv', header = T)
+B_indiv_train <- read.csv('C:/Users/wb516609/Desktop/Poverty-T-competition/B_indiv_train.csv', header = T)
+B_indiv_test <- read.csv('C:/Users/wb516609/Desktop/Poverty-T-competition/B_indiv_test.csv', header = T)
+
+C_hhold_train <- read.csv('C:/Users/wb516609/Desktop/Poverty-T-competition/C_hhold_train.csv', header = T)
+C_hhold_test <- read.csv('C:/Users/wb516609/Desktop/Poverty-T-competition/C_hhold_test.csv', header = T)
+C_indiv_train <- read.csv('C:/Users/wb516609/Desktop/Poverty-T-competition/C_indiv_train.csv', header = T)
+C_indiv_test <- read.csv('C:/Users/wb516609/Desktop/Poverty-T-competition/C_indiv_test.csv', header = T)
